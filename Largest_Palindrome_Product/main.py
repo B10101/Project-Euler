52878e089aec0e92e8b5299c76a985f26ddb4baa
B10101 <b37710101@gmail.com>
2024-05-25 01:54:31 +0300

    Largest Palindrome

--- Largest_Palindrome_Product/main.py
+++ Largest_Palindrome_Product/main.py
@@ -1,9 +1,11 @@
 def palindrome():
+    maxi = []
     for i in range(100,999):
         for j in range(100,999):
             x = str(i*j)
             if x == x[::-1]:
+                maxi.append(int(x))
 
-                print(x)
+    print(max(maxi))
 
 palindrome()
